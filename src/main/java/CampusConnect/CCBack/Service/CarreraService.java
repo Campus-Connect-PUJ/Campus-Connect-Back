1dd6aba6c40ccfbccf153f4a21134e52e2b6dc4f
Alejandro Castro Martinez <alejandro.castro2016@outlook.es>
2021-03-29 16:16:05 -0500

    Fix deployment and compilation

--- src/main/java/CampusConnect/CCBack/Service/CarreraService.java
+++ src/main/java/CampusConnect/CCBack/Service/CarreraService.java
@@ -2,6 +2,8 @@ package CampusConnect.CCBack.Service;
 
 import CampusConnect.CCBack.Model.Carrera;
 
+import CampusConnect.CCBack.Repository.CarreraRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +20,7 @@ class CarreraService {
     }
 
     @GetMapping("/carrera/{id}")
-    public Tip findCarreraById(@PathVariable("id") Long id) {
+    public Carrera findCarreraById(@PathVariable("id") Long id) {
         return repository.findById(id).get();
     }
 }

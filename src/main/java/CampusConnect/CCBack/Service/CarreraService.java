1f0f653f5d96cd7eaafd6743414363e53c072b0b
Alejandro Castro Martinez <alejandro.castro2016@outlook.es>
2021-03-29 15:31:48 -0500

    Carreras Service

---
+++ src/main/java/CampusConnect/CCBack/Service/CarreraService.java
@@ -0,0 +1,24 @@
+package CampusConnect.CCBack.Service;
+
+import CampusConnect.CCBack.Model.Carrera;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class CarreraService {
+    @Autowired
+    private CarreraRepository repository;
+
+    @GetMapping("/carreras")
+    public Iterable<Carrera> findAllCarreras() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/carrera/{id}")
+    public Tip findCarreraById(@PathVariable("id") Long id) {
+        return repository.findById(id).get();
+    }
+}

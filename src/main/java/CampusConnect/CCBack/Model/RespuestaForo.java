b4336432149325cc089b25e00d83f85ee9b0585c
Camilo Muñoz <ancamu99@hotmail.com>
2021-04-11 17:00:26 -0500

    Json ignore de la respuesta del foro

--- src/main/java/CampusConnect/CCBack/Model/RespuestaForo.java
+++ src/main/java/CampusConnect/CCBack/Model/RespuestaForo.java
@@ -8,6 +8,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class RespuestaForo {
 
@@ -19,6 +21,7 @@ public class RespuestaForo {
 
     @ManyToOne
     @JoinColumn(name="idForo")
+	@JsonIgnore
     private Foro foro;
 
     @ManyToOne

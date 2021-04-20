fc42305b51eea4e64074bfd63a00e62181384e20
dhasane <danihas@live.com>
2021-04-20 14:45:45 -0500

    Correcciones menores

--- src/main/java/CampusConnect/CCBack/Controllers/ReglasDeAsociacionController.java
+++ src/main/java/CampusConnect/CCBack/Controllers/ReglasDeAsociacionController.java
@@ -1,10 +1,7 @@
 package CampusConnect.CCBack.Controllers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,11 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import CampusConnect.CCBack.Model.ReglaAsociacionConPuntaje;
 import CampusConnect.CCBack.Model.ReglasDeAsociacion;
 import CampusConnect.CCBack.Model.Tip;
-import CampusConnect.CCBack.Model.UsuarioGeneral;
-import CampusConnect.CCBack.Repository.ReglasDeAsociacionRepository;
 import CampusConnect.CCBack.Service.ReglasDeAsociacionService;
 import CampusConnect.CCBack.Wrappers.WrapperReglaAsociacion;
 
@@ -43,9 +37,13 @@ class ReglasDeAsociacionController {
         return repository.crearReglaDeAsociacion(reglaData);
     }
 
-    @GetMapping("usuario/{id}")
-    public Tip obtenerRecomendacionTip(@PathVariable("id") final Long id){
-        return repository.obtenerRecomendacionTip(id);
+    @GetMapping("usuario")
+    public Tip obtenerRecomendacionTip(
+        @AuthenticationPrincipal String email
+        ){
+        System.out.println(email);
+
+        return repository.obtenerRecomendacionTip(email);
     }
 
 }
